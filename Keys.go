b97452ced3b334f1948acaa61764dc30e6656227
golangframework <server.ozga@gmail.com>
2025-10-02 20:34:07 +0200

    Improvement.

--- Keys.go
+++ Keys.go
@@ -16,7 +16,7 @@ package maps
 func Keys[TSource ~map[TKey]TValue, TKey comparable, TValue any](source TSource) (result []TKey) {
 	result = make([]TKey, len(source))
 	i := 0
-	for key, _ := range source {
+	for key := range source {
 		result[i] = key
 		i++
 	}
